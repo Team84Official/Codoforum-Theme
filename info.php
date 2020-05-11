bc476d1ea054712f2fd07a0f0bcc92771190d62a
Team 84 <team84official@gmail.com>
2020-05-11 18:59:49 -0500

    Initial commit

---
+++ info.php
@@ -0,0 +1,9 @@
+<?php
+
+$info['name'] = 'dark theme';
+$info['description'] = 'Dark theme for CODIF-FORUMS created by Team 84';
+$info['version'] = '1.x';
+$info['author'] = "Team 84 Official";
+$info['author_url'] = 'http://team84official.gq';
+$info['license'] = 'Core License';
+$info['core'] = '1.x';
